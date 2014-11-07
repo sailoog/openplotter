ab2b595f9d704c659333660a93b34603be17106e
sailoog <info@sailoog.com>
2014-11-07 13:33:50 +0100

    Add multilingual

--- openplotter.py
+++ openplotter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import wx, subprocess, socket, pynmea2, time, sys, os, ConfigParser
+import wx, subprocess, socket, pynmea2, time, sys, os, ConfigParser, gettext
 from wx.lib.mixins.listctrl import CheckListCtrlMixin
+import wx.lib.agw.hyperlink as hl
 
-home = os.path.expanduser("~")
+home = os.path.expanduser('~')
 
 class CheckListCtrl(wx.ListCtrl, CheckListCtrlMixin):
 	def __init__(self, parent):
@@ -14,36 +15,53 @@ class MyFrame(wx.Frame):
 		
 		def __init__(self, parent, title):
 
-			wx.Frame.__init__(self, parent, title=title, size=(700,420))
-
+			gettext.install('openplotter', home+'/.config/openplotter/locale', unicode=False)
+			self.presLan_en = gettext.translation('openplotter', home+'/.config/openplotter/locale', languages=['en'])
+			self.presLan_ca = gettext.translation('openplotter', home+'/.config/openplotter/locale', languages=['ca'])
+			self.presLan_es = gettext.translation('openplotter', home+'/.config/openplotter/locale', languages=['es'])
 			self.read_conf()
+			language=self.data_conf.get('GENERAL', 'lang')
+			if language=='en':self.presLan_en.install()
+			if language=='ca':self.presLan_ca.install()
+			if language=='es':self.presLan_es.install()
+
+			wx.Frame.__init__(self, parent, title=title, size=(700,420))
 
 			menubar = wx.MenuBar()
 			self.startup = wx.Menu()
-			self.startup_item1 = self.startup.Append(wx.ID_ANY, 'OpenCPN', 'If selected OpenCPN will run at startup', kind=wx.ITEM_CHECK)
+			self.startup_item1 = self.startup.Append(wx.ID_ANY, _('OpenCPN'), _('If selected OpenCPN will run at startup'), kind=wx.ITEM_CHECK)
 			self.Bind(wx.EVT_MENU, self.check_startup, self.startup_item1)
-			self.startup_item2 = self.startup.Append(wx.ID_ANY, 'NMEA multiplexor (Kplex)', 'If selected Kplex will run at startup', kind=wx.ITEM_CHECK)
+			self.startup_item2 = self.startup.Append(wx.ID_ANY, _('NMEA multiplexor (Kplex)'), _('If selected Kplex will run at startup'), kind=wx.ITEM_CHECK)
 			self.Bind(wx.EVT_MENU, self.check_startup, self.startup_item2)
-			self.startup_item3 = self.startup.Append(wx.ID_ANY, 'Remote desktop (x11vnc)', 'If selected x11vnc will run at startup', kind=wx.ITEM_CHECK)
+			self.startup_item3 = self.startup.Append(wx.ID_ANY, _('Remote desktop (x11vnc)'), _('If selected x11vnc will run at startup'), kind=wx.ITEM_CHECK)
 			self.Bind(wx.EVT_MENU, self.check_startup, self.startup_item3)
-			menubar.Append(self.startup, 'Startup')
+			menubar.Append(self.startup, _('Startup'))
 			time_ = wx.Menu()
-			time_item1 = time_.Append(wx.ID_ANY, 'Set time zone', 'Set time zone in the new window')
+			time_item1 = time_.Append(wx.ID_ANY, _('Set time zone'), _('Set time zone in the new window'))
 			self.Bind(wx.EVT_MENU, self.time_zone, time_item1)
-			time_item2 = time_.Append(wx.ID_ANY, 'Set time from GPS', 'Set system time from GPS')
+			time_item2 = time_.Append(wx.ID_ANY, _('Set time from GPS'), _('Set system time from GPS'))
 			self.Bind(wx.EVT_MENU, self.time_gps, time_item2)
-			menubar.Append(time_, 'Time')
+			menubar.Append(time_, _('Time'))
 			wifi_server = wx.Menu()
-			wifi_server_item1 = wifi_server.Append(wx.ID_ANY, 'Set Server/Client', 'Switch WiFi between "access point" and "DHCP client"')
+			wifi_server_item1 = wifi_server.Append(wx.ID_ANY, _('Set Server/Client'), _('Switch WiFi between "access point" and "DHCP client"'))
 			self.Bind(wx.EVT_MENU, self.OnClick_nmea_server, wifi_server_item1)
-			wifi_server_item2 = wifi_server.Append(wx.ID_ANY, 'Network manager', 'Manage your networks')
+			wifi_server_item2 = wifi_server.Append(wx.ID_ANY, _('Network manager'), _('Manage your networks'))
 			self.Bind(wx.EVT_MENU, self.OnClick_network_man, wifi_server_item2)
-			menubar.Append(wifi_server, 'WiFi')
+			menubar.Append(wifi_server, _('WiFi'))
+			self.lang = wx.Menu()
+			self.lang_item1 = self.lang.Append(wx.ID_ANY, _('English'), _('Set English language'), kind=wx.ITEM_CHECK)
+			self.Bind(wx.EVT_MENU, self.lang_en, self.lang_item1)
+			self.lang_item2 = self.lang.Append(wx.ID_ANY, _('Catalan'), _('Set Catalan language'), kind=wx.ITEM_CHECK)
+			self.Bind(wx.EVT_MENU, self.lang_ca, self.lang_item2)
+			self.lang_item3 = self.lang.Append(wx.ID_ANY, _('Spanish'), _('Set Spanish language'), kind=wx.ITEM_CHECK)
+			self.Bind(wx.EVT_MENU, self.lang_es, self.lang_item3)
+			menubar.Append(self.lang, _('Language'))
+
 			self.SetMenuBar(menubar)
 
 ########################################################
 
-			nmea=wx.StaticBox(self, label=' Add NMEA input / output ', size=(690, 105), pos=(5, 5))
+			nmea=wx.StaticBox(self, label=_(' Add NMEA input / output '), size=(690, 105), pos=(5, 5))
 			estilo = nmea.GetFont()
 			estilo.SetWeight(wx.BOLD)
 			nmea.SetFont(estilo)
@@ -59,12 +77,12 @@ class MyFrame(wx.Frame):
 			self.baudComboBox = wx.ComboBox(self, choices=self.bauds, style=wx.CB_READONLY, size=(90, 30), pos=(215, 30))
 			self.baudComboBox.SetValue('4800')
 
-			wx.StaticText(self, label='<--', pos=(320, 35))
+			wx.StaticText(self, label='<---', pos=(320, 35))
 
-			self.add_serial_in =wx.Button(self, label="Add serial input", pos=(360, 30))
+			self.add_serial_in =wx.Button(self, label=_('Add serial input'), pos=(360, 30))
 			self.Bind(wx.EVT_BUTTON, self.add_serial_input, self.add_serial_in)
 
-			self.add_serial_out =wx.Button(self, label="Add serial output", pos=(520, 30))
+			self.add_serial_out =wx.Button(self, label=_('Add serial output'), pos=(530, 30))
 			self.Bind(wx.EVT_BUTTON, self.add_serial_output, self.add_serial_out)
 
 			self.type = ['TCP', 'UDP']
@@ -75,75 +93,77 @@ class MyFrame(wx.Frame):
 
 			self.port = wx.TextCtrl(self, -1, size=(90, 30), pos=(215, 70))
 
-			wx.StaticText(self, label='<--', pos=(320, 75))
+			wx.StaticText(self, label='<---', pos=(320, 75))
 
-			self.add_network_in =wx.Button(self, label="Add network input", pos=(360, 70))
+			self.add_network_in =wx.Button(self, label=_('Add network input'), pos=(360, 70))
 			self.Bind(wx.EVT_BUTTON, self.add_network_input, self.add_network_in)
 
-			self.add_network_out =wx.Button(self, label="Add network output", pos=(520, 70))
+			self.add_network_out =wx.Button(self, label=_('Add network output'), pos=(530, 70))
 			self.Bind(wx.EVT_BUTTON, self.add_network_output, self.add_network_out)
 
 ########################################################
 
-			in_out=wx.StaticBox(self, label=' NMEA inputs / outputs ', size=(475, 260), pos=(5, 115))
+			in_out=wx.StaticBox(self, label=_(' NMEA inputs / outputs '), size=(475, 260), pos=(5, 115))
 			estilo = in_out.GetFont()
 			estilo.SetWeight(wx.BOLD)
 			in_out.SetFont(estilo)
 
 			self.list_input = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.SUNKEN_BORDER, size=(295, 90), pos=(10, 140))
-			self.list_input.InsertColumn(0, 'Type', width=60)
-			self.list_input.InsertColumn(1, 'Port/Address', width=140)
-			self.list_input.InsertColumn(2, 'Bauds/Port', width=95)
+			self.list_input.InsertColumn(0, _('Type'), width=50)
+			self.list_input.InsertColumn(1, _('Port/Address'), width=130)
+			self.list_input.InsertColumn(2, _('Bauds/Port'), width=115)
 
-			inputs = wx.StaticText(self, label='Inputs', pos=(320, 145))
+			inputs = wx.StaticText(self, label=_('Inputs'), pos=(320, 145))
 			estilo = inputs.GetFont()
 			estilo.SetWeight(wx.BOLD)
 			inputs.SetFont(estilo)
 
-			self.button_delete_input =wx.Button(self, label="Delete selected", pos=(315, 165))
+			self.button_delete_input =wx.Button(self, label=_('Delete selected'), pos=(315, 165))
 			self.Bind(wx.EVT_BUTTON, self.delete_input, self.button_delete_input)
 
 			self.list_output = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.SUNKEN_BORDER, size=(295, 90), pos=(10, 240))
-			self.list_output.InsertColumn(0, 'Type', width=60)
-			self.list_output.InsertColumn(1, 'Port/Address', width=140)
-			self.list_output.InsertColumn(2, 'Bauds/Port', width=95)
+			self.list_output.InsertColumn(0, _('Type'), width=50)
+			self.list_output.InsertColumn(1, _('Port/Address'), width=130)
+			self.list_output.InsertColumn(2, _('Bauds/Port'), width=115)
 
-			outputs = wx.StaticText(self, label='Outputs', pos=(320, 245))
+			outputs = wx.StaticText(self, label=_('Outputs'), pos=(320, 245))
 			estilo = outputs.GetFont()
 			estilo.SetWeight(wx.BOLD)
 			outputs.SetFont(estilo)
 
-			self.button_delete_output =wx.Button(self, label="Delete selected", pos=(315, 265))
+			self.button_delete_output =wx.Button(self, label=_('Delete selected'), pos=(315, 265))
 			self.Bind(wx.EVT_BUTTON, self.delete_output, self.button_delete_output)
 
-			self.show_output =wx.Button(self, label="Show output", pos=(315, 300))
+			self.show_output =wx.Button(self, label=_('Show output'), pos=(315, 300))
 			self.Bind(wx.EVT_BUTTON, self.show_output_window, self.show_output)
 
-			self.restart =wx.Button(self, label="Restart", pos=(10, 337))
+			self.restart =wx.Button(self, label=_('Restart'), pos=(10, 337))
 			self.Bind(wx.EVT_BUTTON, self.restart_multiplex, self.restart)
 
-			self.button_apply =wx.Button(self, label="Apply changes", pos=(120, 337))
+			self.button_apply =wx.Button(self, label=_('Apply changes'), pos=(120, 337))
 			self.Bind(wx.EVT_BUTTON, self.apply_changes, self.button_apply)
 
 ########################################################
 
-			ais_sdr=wx.StaticBox(self, label=' AIS-SDR ', size=(210, 130), pos=(485, 115))
+			ais_sdr=wx.StaticBox(self, label=_(' AIS-SDR '), size=(210, 130), pos=(485, 115))
 			estilo = ais_sdr.GetFont()
 			estilo.SetWeight(wx.BOLD)
 			ais_sdr.SetFont(estilo)
 
-			self.ais_sdr_enable = wx.CheckBox(self, label='Enable', pos=(490, 135))
+			self.ais_sdr_enable = wx.CheckBox(self, label=_('Enable'), pos=(490, 135))
 			self.ais_sdr_enable.Bind(wx.EVT_CHECKBOX, self.OnOffAIS)
 
 			self.gain = wx.TextCtrl(self, -1, size=(50, 30), pos=(490, 165))
-			gain_text = wx.StaticText(self, label='Gain', pos=(550, 170))
+			gain_text = wx.StaticText(self, label=_('Gain'), pos=(550, 170))
 
 			self.ppm = wx.TextCtrl(self, -1, size=(50, 30), pos=(490, 205))
-			ppm_text = wx.StaticText(self, label='Error correction', pos=(550, 210))
+			ppm_text = wx.StaticText(self, label=_('Error correction'), pos=(550, 210))
 
 ########################################################
 
-			self.png = wx.StaticBitmap(self, -1, wx.Bitmap(home+'/.config/openplotter/openplotter500.png', wx.BITMAP_TYPE_ANY), pos=(540, 250))
+			self.png = wx.StaticBitmap(self, -1, wx.Bitmap(home+'/.config/openplotter/openplotter500.png', wx.BITMAP_TYPE_ANY), pos=(500, 250))
+			
+			hyper1 = hl.HyperLinkCtrl(self, -1, 'sailoog.com', URL='http://campus.sailoog.com/course/view.php?id=9', pos=(600, 350))
 
 			self.CreateStatusBar()
 
@@ -206,7 +226,7 @@ class MyFrame(wx.Frame):
 						if 'direction=out' in data[index+1]:
 							output_tmp=[]
 							output_tmp.append('TCP')
-							output_tmp.append('all address')
+							output_tmp.append(_('all address'))
 							item2=self.extract_value(data[index+2])
 							output_tmp.append(item2)
 							self.outputs.append(output_tmp)
@@ -214,7 +234,7 @@ class MyFrame(wx.Frame):
 						if 'direction=in' in data[index+1]:
 							input_tmp=[]
 							input_tmp.append('UDP')
-							input_tmp.append('all address')
+							input_tmp.append(_('all address'))
 							item2=self.extract_value(data[index+2])
 							input_tmp.append(item2)
 							self.inputs.append(input_tmp)
@@ -222,7 +242,7 @@ class MyFrame(wx.Frame):
 				self.write_outputs()
 
 			except IOError:
-				self.SetStatusText('Configuration file does not exist. Add inputs and apply changes')
+				self.SetStatusText(_('Configuration file does not exist. Add inputs and apply changes'))
 
 		def extract_value(self,data):
 			option, value =data.split('=')
@@ -294,7 +314,7 @@ class MyFrame(wx.Frame):
 				self.inputs.append(input_tmp)
 				self.write_inputs()
 			else:
-				self.SetStatusText('It is impossible to set "'+port+'" as input because this port is already in use.')
+				self.SetStatusText(_('It is impossible to set "')+port+_('" as input because this port is already in use.'))
 
 		def add_serial_output(self,event):
 			output_tmp=[]
@@ -312,7 +332,7 @@ class MyFrame(wx.Frame):
 				self.outputs.append(output_tmp)
 				self.write_outputs()
 			else:
-				self.SetStatusText('It is impossible to set "'+port+'" as output because this port is already in use.')
+				self.SetStatusText(_('It is impossible to set "')+port+_('" as output because this port is already in use.'))
 		
 		def add_network_input(self,event):
 			input_tmp=[]
@@ -327,7 +347,7 @@ class MyFrame(wx.Frame):
 				self.inputs.append(input_tmp)
 				self.write_inputs()
 			else:
-				self.SetStatusText('You have to enter at least a port number.')
+				self.SetStatusText(_('You have to enter at least a port number.'))
 		
 		def add_network_output(self,event):
 			output_tmp=[]
@@ -343,17 +363,17 @@ class MyFrame(wx.Frame):
 					self.outputs.append(output_tmp)
 					self.write_outputs()
 				else:
-					self.SetStatusText('Sorry. It is not possible to create UDP outputs.')
+					self.SetStatusText(_('Sorry. It is not possible to create UDP outputs.'))
 			else:
-				self.SetStatusText('You have to enter at least a port number.')
+				self.SetStatusText(_('You have to enter at least a port number.'))
 
 		def OnClick_nmea_server(self,event):
 			subprocess.Popen(['lxterminal', '-e', 'sudo '+home+'/.config/openplotter/nmea_wifi_server/switch_access_point.sh'])
-			self.SetStatusText('Set NMEA server in the new window')
+			self.SetStatusText(_('Set NMEA server in the new window'))
 
 		def OnClick_network_man(self,event):
 			subprocess.Popen('/usr/sbin/wpa_gui')
-			self.SetStatusText('Manage networks in the new window')
+			self.SetStatusText(_('Manage networks in the new window'))
 
 		def OnOffAIS(self, e):
 			sender = e.GetEventObject()
@@ -392,6 +412,10 @@ class MyFrame(wx.Frame):
 			if opencpn=='1': self.startup.Check(self.startup_item1.GetId(), True)
 			if kplex=='1': self.startup.Check(self.startup_item2.GetId(), True)
 			if x11vnc=='1': self.startup.Check(self.startup_item3.GetId(), True)
+			language=self.data_conf.get('GENERAL', 'lang')
+			if language=='en': self.lang.Check(self.lang_item1.GetId(), True)
+			if language=='ca': self.lang.Check(self.lang_item2.GetId(), True)
+			if language=='es': self.lang.Check(self.lang_item3.GetId(), True)
 
 		def write_ais_conf(self):
 			enable_estado=self.ais_sdr_enable.GetValue()
@@ -408,7 +432,7 @@ class MyFrame(wx.Frame):
 		def time_gps(self,event):
 			fecha=""
 			hora=""
-			self.SetStatusText('Waiting for GPS data in localhost:10110 ...')
+			self.SetStatusText(_('Waiting for GPS data in localhost:10110 ...'))
 			try:
 				s = socket.socket()
 				s.connect(("localhost", 10110))
@@ -426,27 +450,27 @@ class MyFrame(wx.Frame):
 						break
 				s.close()
 			except socket.error, error_msg:
-				self.SetStatusText('Failed to connect with localhost:10110. '+'Error code: ' + str(error_msg[0]))
+				self.SetStatusText(_('Failed to connect with localhost:10110. ')+_('Error code: ') + str(error_msg[0]))
 			if (fecha) and (hora):
 				subprocess.call([ 'sudo', 'date', '--set', fecha.strftime('%Y-%m-%d'), '--utc'])
 				subprocess.call([ 'sudo', 'date', '--set', hora.strftime('%H:%M:%S'), '--utc'])
-				self.SetStatusText("Date and time retrieved from GPS successfully")
+				self.SetStatusText(_('Date and time retrieved from GPS successfully'))
 			else:
-				self.SetStatusText("Unable to retrieve date or time from GPS")
+				self.SetStatusText(_('Unable to retrieve date or time from GPS'))
 
 		def time_zone(self,event):
 			subprocess.Popen(['lxterminal', '-e', 'sudo dpkg-reconfigure tzdata'])
-			self.SetStatusText('Set time zone in the new window')
+			self.SetStatusText(_('Set time zone in the new window'))
 		
 		def restart_multiplex(self,event):
 			self.restart_kplex()
 
 		def restart_kplex(self):
-			self.SetStatusText('Closing Kplex')
+			self.SetStatusText(_('Closing Kplex'))
 			subprocess.Popen(["pkill", "kplex"])
 			time.sleep(1)
 			subprocess.Popen('kplex')
-			self.SetStatusText('Kplex restarted')
+			self.SetStatusText(_('Kplex restarted'))
 				
 		def show_output_window(self,event):
 			show_output=subprocess.Popen(['python', home+'/.config/openplotter/output.py'])
@@ -464,8 +488,31 @@ class MyFrame(wx.Frame):
 			with open(home+'/.config/openplotter/openplotter.conf', 'wb') as configfile:
 				self.data_conf.write(configfile)
 
+		def lang_en(self, e):
+			self.lang.Check(self.lang_item1.GetId(), True)
+			self.lang.Check(self.lang_item2.GetId(), False)
+			self.lang.Check(self.lang_item3.GetId(), False)
+			self.lang_selected='en'
+			self.write_lang_selected()
+		def lang_ca(self, e):
+			self.lang.Check(self.lang_item1.GetId(), False)
+			self.lang.Check(self.lang_item2.GetId(), True)
+			self.lang.Check(self.lang_item3.GetId(), False)
+			self.lang_selected='ca'
+			self.write_lang_selected()
+		def lang_es(self, e):
+			self.lang.Check(self.lang_item1.GetId(), False)
+			self.lang.Check(self.lang_item2.GetId(), False)
+			self.lang.Check(self.lang_item3.GetId(), True)
+			self.lang_selected='es'
+			self.write_lang_selected()
+		def write_lang_selected (self):
+			self.data_conf.set('GENERAL', 'lang', self.lang_selected)
+			with open(home+'/.config/openplotter/openplotter.conf', 'wb') as configfile:
+				self.data_conf.write(configfile)
+			self.SetStatusText(_('The selected language will be enabled when you restart'))
 
 
 app = wx.App(False)
-frame = MyFrame(None, 'OpenPlotter Settings')
+frame = MyFrame(None, 'OpenPlotter')
 app.MainLoop()
