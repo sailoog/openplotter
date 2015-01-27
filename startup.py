623f2c3f8ac0c08879b00d9bb60a1734a8450ffc
Sailoog <info@sailoog.com>
2015-01-27 19:25:23 +0100

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import ConfigParser, subprocess, os, sys
+import ConfigParser, subprocess, os, sys, time
 
 pathname = os.path.dirname(sys.argv[0])
 currentpath = os.path.abspath(pathname)
@@ -24,22 +24,32 @@ wifi_server=data_conf.get('WIFI', 'enable')
 wlan=data_conf.get('WIFI', 'device')
 passw=data_conf.get('WIFI', 'password')
 
+if x11vnc=='1':
+	subprocess.Popen(['x11vnc', '-forever'])         
+else: 
+	subprocess.call(['pkill', '-9', 'x11vnc'])
+
+opencpn_commands=[]
+opencpn_commands.append('opencpn')
+if opencpn_no=='1': opencpn_commands.append('-no_opengl')
+if opencpn_fullscreen=='1': opencpn_commands.append('-fullscreen')
+
+if opencpn=='1' and len(opencpn_commands)>1: subprocess.Popen(opencpn_commands)
+if opencpn=='1' and len(opencpn_commands)==1: subprocess.Popen('opencpn')
+
 if enable=='1':
 	rtl_fm=subprocess.Popen(['rtl_fm', '-f', '161975000', '-g', gain, '-p', ppm, '-s', '48k'], stdout = subprocess.PIPE)
 	aisdecoder=subprocess.Popen(['aisdecoder', '-h', '127.0.0.1', '-p', '10110', '-a', 'file', '-c', 'mono', '-d', '-f', '/dev/stdin'], stdin = rtl_fm.stdout)         
 else: 
 	subprocess.call(['pkill', '-9', 'aisdecoder'])
 	subprocess.call(['pkill', '-9', 'rtl_fm'])
-
-if x11vnc=='1':
-	subprocess.Popen(['x11vnc', '-forever'])         
-else: 
-	subprocess.call(['pkill', '-9', 'x11vnc'])
-
+	
 if wifi_server=='1':
 	subprocess.Popen(['sudo', 'python', currentpath+'/wifi_server.py', '1', wlan, passw])
 else:
 	subprocess.Popen(['sudo', 'python', currentpath+'/wifi_server.py', '0', wlan, passw])
+	
+time.sleep(11)
 
 if kplex=='1':
 	subprocess.call(["pkill", '-9', "kplex"])
@@ -55,10 +65,3 @@ if sow=='1':
 else:
 	subprocess.call(['pkill', '-f', 'sog2sow.py'])
 
-opencpn_commands=[]
-opencpn_commands.append('opencpn')
-if opencpn_no=='1': opencpn_commands.append('-no_opengl')
-if opencpn_fullscreen=='1': opencpn_commands.append('-fullscreen')
-
-if opencpn=='1' and len(opencpn_commands)>1: subprocess.Popen(opencpn_commands)
-if opencpn=='1' and len(opencpn_commands)==1: subprocess.Popen('opencpn')
