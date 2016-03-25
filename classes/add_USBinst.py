1cee0bc893396f683a9f0f20bf28993a5986a444
sailoog <info@sailoog.com>
2016-03-25 02:05:20 +0100

    update udev

--- classes/add_USBinst.py
+++ classes/add_USBinst.py
@@ -19,41 +19,7 @@ import wx, pyudev
 class addUSBinst(wx.Dialog):
 
 	def __init__(self):
-		'''
-		first_tty_without_udev = 0
 
-		for device in context.list_devices(subsystem='tty'):
-			if first_tty_without_udev == 1: break
-			for key, value in device.iteritems():
-				if first_tty_without_udev == 1: break
-			
-				if key == 'DEVNAME':
-					value_DEVNAME = value
-				if key == 'DEVLINKS':
-					value_DEVLINKSR= value[value.rfind('/dev/t'):]			
-				if key == 'ID_SERIAL_SHORT':
-					value_ID_SERIAL = value
-				if key == 'ID_SERIAL':
-					value_ID_SERIAL = value
-				if key == 'ID_MODEL_ID':
-					value_ID_MODEL_ID = value
-				if key == 'DEVPATH':
-					value_DEVPATH = value[value.rfind('/usb1/')+6:-(len(value)-value.find('/tty'))]
-					value_DEVPATH = value_DEVPATH[value_DEVPATH.rfind('/')+1:]
-				if key == 'ID_VENDOR_ID':
-					value_ID_VENDOR_ID = value
-					if value_DEVLINKSR == '0':
-						first_tty_without_udev = 1
-						break
-						
-		if first_tty_without_udev == 0:
-			value_DEVNAME=''
-			value_DEVLINKSR=''
-			value_ID_SERIAL=''
-			value_ID_MODEL_ID=''
-			value_DEVPATH=''
-			value_ID_VENDOR_ID = '' 
-		'''
 		wx.Dialog.__init__(self, None, title=_('Rename USB serial port'), size=(580,300))
 
 		self.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
@@ -68,26 +34,7 @@ class addUSBinst(wx.Dialog):
 		self.list_devices.InsertColumn(3, _('port'), width=100)
 		self.list_devices.InsertColumn(4, _('serial'), width=230)
 		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.select_device, self.list_devices)
-		'''
-		list_OPnames=['ttyOP_Plotter','ttyOP_Radio','ttyOP_AIS','ttyOP_GPS','ttyOP_AUTOPILOT','ttyOP_NMEA1','ttyOP_NMEA2','ttyOP_NMEA3','ttyOP_SEATALK','ttyOP_NMEA2000',' ']
-		
-		wx.StaticText(panel, label=_('OP name'), pos=(10, 10))
-		self.OPname_select= wx.ComboBox(panel, choices=list_OPnames, style=wx.CB_READONLY, size=(310, 30), pos=(10, 35))
 
-		wx.StaticText(panel, label=_('vendor'), pos=(10, 70))
-		self.vendor = wx.TextCtrl(panel, value=value_ID_VENDOR_ID, style=wx.CB_READONLY, size=(100, 30), pos=(10, 95))
-		
-		wx.StaticText(panel, label=_('serial'), pos=(160, 70))
-		if value_ID_SERIAL[:len(value_ID_VENDOR_ID)] == value_ID_VENDOR_ID:
-			value_ID_SERIAL = ''
-		self.serial = wx.TextCtrl(panel, value=value_ID_SERIAL, style=wx.CB_READONLY, size=(100, 30), pos=(160, 95))
-
-		wx.StaticText(panel, label=_('product'), pos=(10, 130))
-		self.product = wx.TextCtrl(panel, value=value_ID_MODEL_ID, style=wx.CB_READONLY, size=(100, 30), pos=(10, 155))
-
-		wx.StaticText(panel, label=_('connect port'), pos=(160, 130))
-		self.con_port = wx.TextCtrl(panel, value=value_DEVPATH, style=wx.CB_READONLY, size=(100, 30), pos=(160, 155))
-		'''
 		wx.StaticText(panel, label=_('new name'), pos=(15, 140))
 		self.OPname_label=wx.StaticText(panel, label='/dev/ttyOP_', pos=(15, 170))
 		self.OPname_select= wx.TextCtrl(panel, size=(100, 30), pos=(90, 160))
@@ -103,7 +50,6 @@ class addUSBinst(wx.Dialog):
 		self.rem_port = wx.CheckBox(panel, label=_('Remember port'), pos=(245, 165))	
 		self.rem_port.Bind(wx.EVT_CHECKBOX, self.on_enable_port)
 		self.rem_port.Disable()
-
 
 		context = pyudev.Context()
 		for device in context.list_devices(subsystem='tty'):
