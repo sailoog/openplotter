b8abab7c0c6ee27905d924be06286021e46b2fd8
sailoog <info@sailoog.com>
2016-06-20 18:30:48 +0200

    Convert SDR-AIS into a Tool

--- openplotter.py
+++ openplotter.py
@@ -63,7 +63,7 @@ class MainFrame(wx.Frame):
 		self.page1 = wx.Panel(self.nb)
 		self.page2 = wx.Panel(self.nb)
 		self.page3 = wx.Panel(self.nb)
-		self.page4 = wx.Panel(self.nb)
+		#self.page4 = wx.Panel(self.nb)
 		self.page5 = wx.Panel(self.nb)
 		self.page6 = wx.Panel(self.nb)
 		self.page7 = wx.Panel(self.nb)
@@ -88,7 +88,7 @@ class MainFrame(wx.Frame):
 		self.nb.AddPage(self.page6, _('I2C sensors'))
 		self.nb.AddPage(self.page11, _('1W sensors'))
 		self.nb.AddPage(self.page12, _('SPI sensors'))
-		self.nb.AddPage(self.page4, _('SDR-AIS'))
+		#self.nb.AddPage(self.page4, _(''))
 		self.nb.AddPage(self.page2, _('Calculate'))
 		self.nb.AddPage(self.page9, _('Accounts'))
 		self.nb.AddPage(self.page16, _('MQTT'))
@@ -113,7 +113,10 @@ class MainFrame(wx.Frame):
 		self.settings.AppendSeparator()
 		self.gpsd_item1 = self.settings.Append(wx.ID_ANY, _('Set GPSD'), _('Set GPSD in the new window'))
 		self.Bind(wx.EVT_MENU, self.reconfigure_gpsd, self.gpsd_item1)
-		self.menubar.Append(self.settings, _('Settings'))
+		self.settings.AppendSeparator()
+		self.sdr_ais_item1 = self.settings.Append(wx.ID_ANY, _('SDR-AIS receiver'), _('Set an SDR-AIS receiver in the new window'))
+		self.Bind(wx.EVT_MENU, self.open_sdr_ais, self.sdr_ais_item1)
+		self.menubar.Append(self.settings, _('Tools'))
 
 		self.lang = wx.Menu()
 		self.lang_item1 = self.lang.Append(wx.ID_ANY, _('English'), _('Set English language'), kind=wx.ITEM_CHECK)
@@ -274,48 +277,7 @@ class MainFrame(wx.Frame):
 		self.Bind(wx.EVT_BUTTON, self.show_ip_info, self.button_refresh_ip)
 ###########################page3
 ########page4###################
-		wx.StaticBox(self.page4, label='', size=(400, 170), pos=(10, 10))
 
-		self.ais_sdr_enable = wx.CheckBox(self.page4, label=_('Enable AIS NMEA generation'), pos=(20, 25))
-		self.ais_sdr_enable.Bind(wx.EVT_CHECKBOX, self.OnOffAIS)
-
-		self.gain = wx.TextCtrl(self.page4, -1, size=(55, 32), pos=(150, 60))
-		self.gain_label=wx.StaticText(self.page4, label=_('Gain'), pos=(20, 65))
-		self.ppm = wx.TextCtrl(self.page4, -1, size=(55, 32), pos=(150, 95))
-		self.correction_label=wx.StaticText(self.page4, label=_('Correction (ppm)'), pos=(20, 100))
-
-		self.ais_frequencies1 = wx.CheckBox(self.page4, label=_('Channel A 161.975Mhz'), pos=(220, 60))
-		self.ais_frequencies1.Bind(wx.EVT_CHECKBOX, self.ais_frequencies)
-		self.ais_frequencies2 = wx.CheckBox(self.page4, label=_('Channel B 162.025Mhz'), pos=(220, 95))
-		self.ais_frequencies2.Bind(wx.EVT_CHECKBOX, self.ais_frequencies)
-
-		self.show_kplex6 =wx.Button(self.page4, label=_('Inspector'), pos=(20, 140))
-		self.Bind(wx.EVT_BUTTON, self.show_kplex, self.show_kplex6)
-		self.button_test_ppm =wx.Button(self.page4, label=_('Take a look'), pos=(150, 140))
-		self.Bind(wx.EVT_BUTTON, self.test_ppm, self.button_test_ppm)
-		self.button_test_gain =wx.Button(self.page4, label=_('Calibration'), pos=(275, 140))
-		self.Bind(wx.EVT_BUTTON, self.test_gain, self.button_test_gain)
-
-		wx.StaticBox(self.page4, label=_(' Fine calibration using GSM '), size=(260, 170), pos=(420, 10))
-		self.bands_label=wx.StaticText(self.page4, label=_('Band'), pos=(430, 50))
-		self.bands_list = ['GSM850', 'GSM-R', 'GSM900', 'EGSM', 'DCS', 'PCS']
-		self.band= wx.ComboBox(self.page4, choices=self.bands_list, style=wx.CB_READONLY, size=(100, 32), pos=(430, 70))
-		self.band.SetValue('GSM900')
-		self.check_bands =wx.Button(self.page4, label=_('Check band'), pos=(540, 70))
-		self.Bind(wx.EVT_BUTTON, self.check_band, self.check_bands)
-		self.channel_label=wx.StaticText(self.page4, label=_('Channel'), pos=(430, 125))
-		self.channel = wx.TextCtrl(self.page4, -1, size=(55, 32), pos=(430, 143))
-		self.check_channels =wx.Button(self.page4, label=_('Fine calibration'), pos=(495, 140))
-		self.Bind(wx.EVT_BUTTON, self.check_channel, self.check_channels)
-
-		wx.StaticBox(self.page4, label=_(' VHF '), size=(260, 130), pos=(420, 185))
-
-		self.button_vhf_Rx =wx.Button(self.page4, label=_('Receive'), pos=(430, 210))
-		self.Bind(wx.EVT_BUTTON, self.vhf_Rx, self.button_vhf_Rx)
-
-		self.button_vhf_Tx =wx.Button(self.page4, label=_('Transmit'), pos=(430, 255))
-		self.Bind(wx.EVT_BUTTON, self.vhf_Tx, self.button_vhf_Tx)
-		self.Tx_exp_label=wx.StaticText(self.page4, label=_('Experimental'), pos=(540, 263))
 
 ###########################page4
 ########page5###################
@@ -788,36 +750,6 @@ class MainFrame(wx.Frame):
 		if self.conf.get('STARTUP', 'play')=='1':
 			self.startup_play_sound.SetValue(True) 
 
-		output=subprocess.check_output('lsusb')
-		supported_dev=['0bda:2832','0bda:2838','0ccd:00a9','0ccd:00b3','0ccd:00d3','0ccd:00e0','185b:0620','185b:0650','1f4d:b803','1f4d:c803','1b80:d3a4','1d19:1101','1d19:1102','1d19:1103','0458:707f','1b80:d393','1b80:d394','1b80:d395','1b80:d39d']
-		found=False
-		for i in supported_dev:
-			if i in output:found=True
-		if found:
-			self.gain.SetValue(self.conf.get('AIS-SDR', 'gain'))
-			self.ppm.SetValue(self.conf.get('AIS-SDR', 'ppm'))
-			self.band.SetValue(self.conf.get('AIS-SDR', 'band'))
-			self.channel.SetValue(self.conf.get('AIS-SDR', 'gsm_channel'))
-			if self.conf.get('AIS-SDR', 'enable')=='1': 
-				self.ais_sdr_enable.SetValue(True)
-				self.disable_sdr_controls()
-			if self.conf.get('AIS-SDR', 'channel')=='a': self.ais_frequencies1.SetValue(True)
-			if self.conf.get('AIS-SDR', 'channel')=='b': self.ais_frequencies2.SetValue(True)
-		else:
-			self.ais_sdr_enable.Disable()
-			self.disable_sdr_controls()
-			self.button_test_gain.Disable()
-			self.button_test_ppm.Disable()
-			self.bands_label.Disable()
-			self.channel_label.Disable()
-			self.band.Disable()
-			self.channel.Disable()
-			self.check_channels.Disable()
-			self.check_bands.Disable()
-			self.button_vhf_Tx.Disable()
-			self.Tx_exp_label.Disable()
-			self.button_vhf_Rx.Disable()
-
 		self.rate.SetValue(self.conf.get('STARTUP', 'nmea_rate_sen'))
 		self.rate2.SetValue(self.conf.get('STARTUP', 'nmea_rate_cal'))
 		self.accuracy.SetValue(self.conf.get('STARTUP', 'cal_accuracy'))
@@ -929,6 +861,10 @@ class MainFrame(wx.Frame):
 	def reconfigure_gpsd(self,event):
 		subprocess.Popen(['lxterminal', '-e', 'sudo nano /etc/default/gpsd'])
 		self.SetStatusText(_('Set GPSD in the new window'))
+
+	def open_sdr_ais(self,event):
+		close=subprocess.call(['pkill', '-f', 'sdr_ais.py'])
+		show_output=subprocess.Popen(['python',currentpath+'/sdr_ais.py'])
 	
 	def clear_lang(self):
 		self.lang.Check(self.lang_item1.GetId(), False)
@@ -1335,124 +1271,6 @@ along with this program.  If not, see http://www.gnu.org/licenses/"""
 			self.bridge_enable.Enable()
 			self.conf.set('WIFI', 'enable', '0')
 
-###########################################	SDR-AIS
-
-	def kill_sdr(self):
-		subprocess.call(['pkill', '-9', 'aisdecoder'])
-		subprocess.call(['pkill', '-9', 'rtl_fm'])
-		subprocess.call(['pkill', '-f', 'waterfall.py'])
-		subprocess.call(['pkill', '-9', 'rtl_test'])
-		subprocess.call(['pkill', '-9', 'kal'])
-		subprocess.call(['pkill', '-9', 'qtcsdr'])
-
-	def enable_sdr_controls(self):
-		self.gain.Enable()
-		self.ppm.Enable()
-		self.ais_frequencies1.Enable()
-		self.ais_frequencies2.Enable()
-		self.gain_label.Enable()
-		self.correction_label.Enable()
-		self.ais_sdr_enable.SetValue(False)
-		self.conf.set('AIS-SDR', 'enable', '0')
-
-	def disable_sdr_controls(self):
-		self.gain.Disable()
-		self.ppm.Disable()
-		self.ais_frequencies1.Disable()
-		self.ais_frequencies2.Disable()
-		self.gain_label.Disable()
-		self.correction_label.Disable()
-	
-	def ais_frequencies(self, e):
-		sender = e.GetEventObject()
-		self.ais_frequencies1.SetValue(False)
-		self.ais_frequencies2.SetValue(False)
-		sender.SetValue(True)
-
-	def OnOffAIS(self, e):
-		self.kill_sdr()
-		isChecked = self.ais_sdr_enable.GetValue()
-		if isChecked:
-			self.disable_sdr_controls() 
-			gain=self.gain.GetValue()
-			ppm=self.ppm.GetValue()
-			frecuency='161975000'
-			channel='a'
-			if self.ais_frequencies2.GetValue(): 
-				frecuency='162025000'
-				channel='b'
-			rtl_fm=subprocess.Popen(['rtl_fm', '-f', frecuency, '-g', gain, '-p', ppm, '-s', '48k'], stdout = subprocess.PIPE)
-			aisdecoder=subprocess.Popen(['aisdecoder', '-h', '127.0.0.1', '-p', '10110', '-a', 'file', '-c', 'mono', '-d', '-f', '/dev/stdin'], stdin = rtl_fm.stdout)         
-			self.conf.set('AIS-SDR', 'enable', '1')
-			self.conf.set('AIS-SDR', 'gain', gain)
-			self.conf.set('AIS-SDR', 'ppm', ppm)
-			self.conf.set('AIS-SDR', 'channel', channel)
-			msg=_('SDR-AIS reception enabled')
-		else:
-			self.enable_sdr_controls()
-			self.conf.set('AIS-SDR', 'enable', '0')
-			msg=_('SDR-AIS reception disabled')
-		self.SetStatusText(msg)
-
-	def test_ppm(self,event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		gain='25'
-		if self.gain.GetValue():
-			gain=self.gain.GetValue()
-			gain=gain.replace(',', '.')
-		ppm='0'
-		if self.ppm.GetValue():
-			ppm=self.ppm.GetValue()
-			ppm=ppm.replace(',', '.')
-		channel='a'
-		if self.ais_frequencies2.GetValue(): channel='b'
-		w_open=subprocess.Popen(['python', currentpath+'/waterfall.py', gain, ppm, channel])
-		msg=_('SDR-AIS reception disabled. After closing the new window enable SDR-AIS reception again.')
-		self.SetStatusText(msg)
-
-	def test_gain(self,event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		subprocess.Popen(['lxterminal', '-e', 'rtl_test', '-p'])
-		msg=_('SDR-AIS reception disabled.\nCheck the new window. Copy the maximum supported gain value. Wait for ppm value to stabilize and copy it too.')
-		self.ShowMessage(msg)
-
-	def check_band(self, event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		gain=self.gain.GetValue()
-		ppm=self.ppm.GetValue()
-		band=self.band.GetValue()
-		self.conf.set('AIS-SDR', 'gain', gain)
-		self.conf.set('AIS-SDR', 'ppm', ppm)
-		self.conf.set('AIS-SDR', 'band', band)
-		subprocess.Popen(['python',currentpath+'/fine_cal.py', 'b'])
-
-	def check_channel(self, event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		gain=self.gain.GetValue()
-		ppm=self.ppm.GetValue()
-		channel=self.channel.GetValue()
-		self.conf.set('AIS-SDR', 'gain', gain)
-		self.conf.set('AIS-SDR', 'ppm', ppm)
-		self.conf.set('AIS-SDR', 'gsm_channel', channel)
-		if channel: subprocess.Popen(['python',currentpath+'/fine_cal.py', 'c'])
-
-	def vhf_Rx(self, event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		subprocess.Popen('qtcsdr')
-		msg=_('SDR-AIS reception disabled. After closing the new window enable SDR-AIS reception again.')
-		self.SetStatusText(msg)
-
-	def vhf_Tx(self, event):
-		self.kill_sdr()
-		self.enable_sdr_controls()
-		subprocess.Popen(['lxterminal', '-e', currentpath+'/classes/rpi-test.sh'])
-		msg=_('SDR-AIS reception disabled. After closing the new window enable SDR-AIS reception again.')
-		self.SetStatusText(msg)
 ###########################################	NMEA 0183
 
 	def show_kplex(self,event):
