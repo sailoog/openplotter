4fa6950151257ac559ae081696e2f5ed4c5711bf
e-sailing <martin.stell@freenet.de>
2016-07-27 19:11:05 +0200

    Analog to SignalK as tool

---
+++ tools/analog_ads1115.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+# This file is part of Openplotter.
+# Copyright (C) 2015 by sailoog <https://github.com/sailoog/openplotter>
+#
+# Openplotter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+# Openplotter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Openplotter. If not, see <http://www.gnu.org/licenses/>.
+
+import socket, time, RTIMU, math, csv, datetime, json, subprocess, sys
+from classes.paths import Paths
+from classes.ads1115 import Ads1115
+from classes.conf_analog import Conf_analog
+
+paths=Paths()
+currentpath=paths.currentpath
+home=paths.home
+
+if len(sys.argv)>1:
+	if sys.argv[1]=='settings':
+		subprocess.Popen(['leafpad',currentpath+'/openplotter_analog.conf'])
+	exit
+else:
+
+	conf_analog=Conf_analog()
+	ads1115=Ads1115()
+
+	a_index = [0,1,2,3]
+	a_value =[0.0,0.0,0.0,0.0]
+	index=0
+	active=['0','0','0','0']
+	SK_name=['0','0','0','0']
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	poll_interval = 1
+	rate_analog = 1
+	ADS1115='ADS1115_'
+
+	for i in a_index:
+		active[i] = conf_analog.get(ADS1115+str(i), 'active')=='1'
+
+		if active[i]:
+			if 0==conf_analog.has_option(ADS1115+str(i), 'sk_name'):
+				conf_analog.set(ADS1115+str(i), 'sk_name','0')
+
+			SK_name[i] = conf_analog.get(ADS1115+str(i), 'sk_name')
+
+	with open('/home/pi/.config/signalk-server-node/settings/openplotter-settings.json') as data_file:
+		data = json.load(data_file)
+	uuid=data['vessel']['uuid']
+
+	tick_analog=time.time()
+
+	while True:
+		tick2=time.time()
+		time.sleep(poll_interval*1.0/1000.0)
+		#GENERATE	
+
+		if tick2-tick_analog > rate_analog:
+			tick_analog=time.time()
+
+			list_signalk_path1=[]
+			list_signalk1=[]
+
+			for i in a_index:
+				if active[i]:	
+					list_signalk_path1.append(SK_name[i])
+					list_signalk1.append(str(a_value[i]))			
+
+			if list_signalk1:
+				SignalK = '{"updates": [{"source": {"type": "I2C","src" : "ANA"},"timestamp": "'+str( datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f') )[0:23]+'Z","values":[ '
+				Erg=''
+				for i in range(0,len(list_signalk1)):
+					Erg += '{"path": "'+list_signalk_path1[i]+'","value":'+list_signalk1[i]+'},'
+				SignalK+=Erg[0:-1]+']}],"context": "vessels.'+uuid+'"}\n'
+				sock.sendto(SignalK, ('127.0.0.1', 55556))			
+		else:
+			index+=1
+			if index>3: index=0
+
+			a_value[index]=ads1115.read(index)
---
+++ tools/oppymata.py
@@ -0,0 +1,125 @@
+import signal, sys, time, socket, datetime,json,subprocess
+
+from PyMata.pymata import PyMata
+from classes.conf_analog import Conf_analog
+from classes.paths import Paths
+
+
+# Control-C signal handler to suppress exceptions if user presses Control C
+# This is totally optional.
+def signal_handler(sig, frame):
+	print('You pressed Ctrl+C!!!!')
+	if board is not None:
+		board.reset()
+	sys.exit(0)
+
+def interpolread(idx,erg):
+	if adjust_point_active[idx]:
+		lin = -999999
+		for index,item in enumerate(adjust_point[idx]):
+			if index==0:
+				if erg <= item[0]:
+					lin = item[1]
+					#print 'under range'
+					return lin
+				save = item
+			else:					
+				if erg <= item[0]:
+					a = (item[1]-save[1])/(item[0]-save[0])
+					b = item[1]-a*item[0]
+					lin = a*erg +b
+					return lin
+				save = item
+				
+		if lin == -999999:
+			#print 'over range'
+			lin = save[1]
+		return lin
+	else:
+		return erg
+		
+def init():	
+	signal.signal(signal.SIGINT, signal_handler)
+
+	conf_analog=Conf_analog()
+
+	global uuid
+	with open('/home/pi/.config/signalk-server-node/settings/openplotter-settings.json') as data_file:
+		data = json.load(data_file)
+	uuid=data['vessel']['uuid']
+
+
+	FIRMATA='FIRMATA_'
+
+	#example to set arduino A2 as analog input -> config file "openplotter_analog.conf"
+	#[FIRMATA_2]
+	#sk_name = tanks.fuel.left.currentLevel
+	#adjust_points = [[403.0,0.0],[522.0,25.0],[708.0,50.0],[913.0,100],[1024.0,100.01]]	
+	
+	index=0
+	for i in range(16):
+		if conf_analog.has_section(FIRMATA+str(i)):
+			channel.append(i)
+			RawValue.append(0)
+			adjust_point_active.append(0)
+			adjust_point.append(0)
+			channel_index.append(index)
+			board.set_pin_mode(i, board.INPUT, board.ANALOG)
+			if 0==conf_analog.has_option(FIRMATA+str(i), 'sk_name'):
+				conf_analog.set(FIRMATA+str(i), 'sk_name','0')
+
+			SK_name.append(conf_analog.get(FIRMATA+str(i), 'sk_name'))
+			
+			if 0==conf_analog.has_option(FIRMATA+str(i), 'adjust_points'):
+				adjust_point_active[index] = 0
+			else:
+				line = conf_analog.get(FIRMATA+str(i), 'adjust_points')			
+				if line: 
+					adjust_point[index]=eval(line)
+					adjust_point_active[index] = 1
+				else: adjust_point[index]=[]
+			index+=1
+
+
+paths=Paths()
+currentpath=paths.currentpath
+
+if len(sys.argv)>1:
+	if sys.argv[1]=='settings':
+		subprocess.Popen(['leafpad',currentpath+'/openplotter_analog.conf'])
+	exit
+else:
+	RawValue=[]
+	adjust_point=[]
+	adjust_point_active=[]
+	channel=[]
+	channel_index=[]
+	SK_name=[]
+	index=0
+	SignalK=''
+	board = PyMata("/dev/ttyOP_FIRM")
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				
+	init()
+	index=0
+	length=len(channel_index)-1
+		
+	# A forever loop until user presses Control-C
+	while 1:	
+		RawValue[index]=board.analog_read(channel[index])
+		time.sleep(0.100)
+
+		index+=1
+		if index > length:
+			index=0
+
+			SignalK = '{"updates": [{"source": {"type": "ARD","src" : "ANA"},"timestamp": "'+str( datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f') )[0:23]+'Z","values":[ '
+			Erg=''
+			for i in channel_index:
+				Erg += '{"path": "'+SK_name[i]+'","value":'+str(interpolread(i,RawValue[i]))+'},'
+			
+			SignalK +=Erg[0:-1]+']}],"context": "vessels.'+uuid+'"}\n'
+			#print SignalK
+			sock.sendto(SignalK, ('127.0.0.1', 55557))
+			time.sleep(0.100)
+			
