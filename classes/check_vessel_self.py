0b4c6c48e2277f07093c14a4746ad0a3d981b751
sailoog <info@sailoog.com>
2016-11-18 20:37:27 +0100

    fix issue with uuid

--- classes/check_vessel_self.py
+++ classes/check_vessel_self.py
@@ -68,16 +68,9 @@ class checkVesselSelf:
 
 		response = requests.get('http://localhost:3000/signalk/v1/api/vessels/self')
 		data = response.json()
-		self.uuid = data['uuid']
-		if 'mmsi' in self.uuid:
-			sp = self.uuid.split(':')
-			self.mmsi = sp[len(sp) - 1]
-		else:
-			try:
-				self.mmsi = data['mmsi']
-			except:
-				self.mmsi = '0000'
-				print 'Error: No mmsi! mmsi has to be set'
+		self.mmsi = data['mmsi']
+		self.uuid = 'urn:mrn:imo:mmsi:'+self.mmsi
+
 
 	def util_process_exist(self, process_name):
 		pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
