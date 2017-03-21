4912f531efb6d2f1900d3c005c23afbbf0c105aa
sailoog <info@sailoog.com>
2017-03-21 20:16:31 +0100

    Updating OpenCPN from OP interface

---
+++ update/update_OpenCPN.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo
+echo "UPDATING PACKAGES..."
+echo
+sudo apt-get update
+echo
+echo "INSTALLING OPENCPN..."
+echo
+sudo apt-get install opencpn
+echo
+echo "EDITING OPENCPN MENU..."
+echo
+sudo bash -c 'echo -e "[Desktop Entry]\nName=OpenCPN\nComment=Open source chart plotter / navigator\nExec=opencpn\nIcon=opencpn\nStartupNotify=true\nCategories=OpenPlotter\nTerminal=false\nType=Application\nVersion=1.0" > /usr/share/applications/opencpn.desktop'
+echo
+read -p "FINISHED PRESS ENTER TO EXIT"
