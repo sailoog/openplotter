48cfd87bdebd4008d07be0b5ed738e43460a206a
Sailoog <info@sailoog.com>
2017-08-04 12:46:19 +0200

    Update check_rtimulib.py

--- check_rtimulib.py
+++ check_rtimulib.py
@@ -26,7 +26,7 @@ try:
 	s = RTIMU.Settings(SETTINGS_FILE)
 	imu = RTIMU.RTIMU(s)
 
-	result = [[0,0], [0,0], [0,0]]
+	result = [['',''], ['',''], ['','']]
 
 	with open(SETTINGS_FILE+'.ini', "r") as infile:
 		for line in infile:
