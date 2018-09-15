82faff9ced389dd1b085a95d4d68b0a8e7d1d399
sailoog <info@sailoog.com>
2018-09-15 21:14:26 +0200

    add button to install wifi drivers

---
+++ classes/install-wifi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo
+echo "Adding support for wifi modules using the rtl8188eu, rtl8192eu, rtl8812au, 8822bu, mt7610 and mt7612 drivers..."
+echo
+sudo install-wifi
+echo
+read -p "Press enter to close this window"
