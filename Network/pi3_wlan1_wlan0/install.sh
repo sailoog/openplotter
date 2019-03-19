d743058a82c1eb4306dcaebfd8fb691075b3720e
e-sailing <martin.stell@freenet.de>
2019-03-19 16:37:05 +0100

    Network update

--- Network/pi3_wlan1_wlan0/install.sh
+++ Network/pi3_wlan1_wlan0/install.sh
@@ -5,14 +5,9 @@ sudo cp sysctl.conf /etc
 sudo cp .openplotter/start-ap-managed-wifi.sh ~/.openplotter
 sudo cp hostapd/hostapd.conf /etc/hostapd
 sudo cp network/interfaces /etc/network
+sudo cp network/interfaces.d/ap /etc/network/interfaces.d
 sudo cp network/interfaces.d/station /etc/network/interfaces.d
 sudo cp udev/rules.d/72-static-name.rules /etc/udev/rules.d
-
-
-if [ -e /etc/network/interfaces.d/ap ]
-then
-    sudo rm /etc/network/interfaces.d/ap
-fi
 
 if [ -e /etc/udev/rules.d/90-wireless.rules ]
 then
@@ -23,3 +18,24 @@ if [ -e /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant ]
 then
     sudo mv /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant /lib/dhcpcd
 fi
+
+if [ ! -e /etc/udev/rules.d/11-openplotter-usb0.rules ]
+then
+    sudo cp udev/rules.d/11-openplotter-usb0.rules /etc/udev/rules.d
+fi
+
+erg=$(systemctl status dnsmasq | grep disabled)
+chrlen=${#erg}
+if [ $chrlen -gt 0 ]
+then
+	sudo systemctl enable dnsmasq
+fi
+
+erg=$(systemctl status hostapd | grep disabled)
+chrlen=${#erg}
+if [ $chrlen -gt 0 ]
+then
+	sudo systemctl enable hostapd
+fi
+
+/bin/bash add_hostname_dot_local.sh
