5bb17f3ffe149e523f4fe68e9d452cbfe051c05e
Sailoog <info@sailoog.com>
2019-04-27 15:24:31 +0200

    rebuild SDR management

--- tools/SDR_AIS/SDR_AIS_fine_cal.py
+++ tools/SDR_AIS/SDR_AIS_fine_cal.py
@@ -35,37 +35,37 @@ class MainFrame(wx.Frame):
 
 			Language(self.conf)
 
-			wx.Frame.__init__(self, None, title=_('Fine calibration'), size=(500,300))
+			wx.Frame.__init__(self, None, title=_('Fine calibration'), size=(600,320))
 
 			self.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
 			
 			self.icon = wx.Icon(self.currentpath+'/static/icons/openplotter.ico', wx.BITMAP_TYPE_ICO)
 			self.SetIcon(self.icon)
 
-			self.CreateStatusBar()
-
 			self.text=wx.StaticText(self, label=_('Error'), pos=(10, 10))
 
-			self.gain=self.conf.get('AIS-SDR', 'gain')
 			self.ppm=self.conf.get('AIS-SDR', 'ppm')
+			self.band=self.conf.get('AIS-SDR', 'band')
 			self.channel=self.conf.get('AIS-SDR', 'gsm_channel')
 
-			wx.StaticText(self, label=_('gain: ').decode('utf8')+self.gain, pos=(10, 70))
-			wx.StaticText(self, label=_('ppm: ').decode('utf8')+self.ppm, pos=(100, 70))
+			wx.StaticText(self, label=_('Initial PPM: ').decode('utf8')+self.ppm, pos=(10, 80))
 
-			self.output = wx.TextCtrl(self, style=wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_DONTWRAP, size=(480,110), pos=(10,90))
+			self.output = wx.TextCtrl(self, style=wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_DONTWRAP, size=(580,120), pos=(10,100))
 			
-			self.button_close =wx.Button(self, label=_('Close'), pos=(300, 210))
+			self.button_close =wx.Button(self, label=_('Close'), pos=(400, 230))
 			self.Bind(wx.EVT_BUTTON, self.close, self.button_close)
 
-			self.button_calculate =wx.Button(self, label=_('Calculate'), pos=(400, 210))
+			self.button_calculate =wx.Button(self, label=_('Start'), pos=(500, 230))
 			self.Bind(wx.EVT_BUTTON, self.calculate, self.button_calculate)
-			
+
 			if self.option=='c': 
-				self.text.SetLabel(_('Press Calculate and wait for the system to calculate the ppm value with\nthe selected channel. Put the obtained value in "Correction (ppm)" field\nand enable SDR-AISreception. Estimated time: 1 min.'))
-				wx.StaticText(self, label=_('channel: ').decode('utf8')+self.channel, pos=(200, 70))
+				self.text.SetLabel(_('Press Start and wait for the system to calculate the PPM value.\nRound the resulting PPM to the nearest integer and copy it into "PPM" field.\nEstimated time: 1 min.'))
+				wx.StaticText(self, label=_('channel: ').decode('utf8')+self.channel, pos=(200, 80))
 			if self.option=='b':
-				self.text.SetLabel(_('Press Calculate and wait for the system to check the band. Write down\nthe strongest channel (power). If you do not find any channel try another\nband. Estimated time: 5 min.'))
+				self.text.SetLabel(_('Press Start and wait for the system to check the band.\nWrite down the strongest channel (power).\nIf you do not find any channel try another band.\nEstimated time: 5 min.'))
+				wx.StaticText(self, label=_('band: ').decode('utf8')+self.band, pos=(200, 80))
+
+			self.CreateStatusBar()
 
 			self.Centre()
  	
@@ -73,13 +73,11 @@ class MainFrame(wx.Frame):
 			self.SetStatusText(_('Working...'))
 			self.output.SetValue('')
 			if self.option=='c': 
-				
-				try: output=subprocess.check_output(['kal', '-c', self.channel, '-g', self.gain, '-e', self.ppm])
-				except: output=_('error')
+				try: output=subprocess.check_output(['kal', '-c', self.channel, '-e', self.ppm])
+				except Exception, e: output = _("Error: ")+str(e)
 			if self.option=='b': 
-				band=self.conf.get('AIS-SDR', 'band')
-				try: output=subprocess.check_output(['kal', '-s', band, '-g', self.gain, '-e', self.ppm])
-				except: output=_('error')
+				try: output=subprocess.check_output(['kal', '-s', self.band, '-e', self.ppm])
+				except Exception, e: output = _("Error: ")+str(e)
 			self.output.SetValue(output)
 			self.SetStatusText(_('Finished'))
 
